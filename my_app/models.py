808a5e68216879e4dc3ae6fbce7f2b0308180322
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-06 19:54:17 +0000

    added class Search

--- my_app/models.py
+++ my_app/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
+
+    #Will show the real string instead of object(number) 
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    class Meta:
+        verbose_name_plural ='Searches'
