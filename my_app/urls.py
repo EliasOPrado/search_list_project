c38288796c2a3f912f807a84def5410e100b707c
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-06 21:41:45 +0000

    added new_search class and template

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.home, name='home'),
+    url(r'^new_search', views.new_search, name='new_search')
 ]
