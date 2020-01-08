8204fd8fe9a6c843c8220e88bcbd21b06a5346eb
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-08 14:17:12 +0000

    added search functionality

--- my_app/views.py
+++ my_app/views.py
@@ -1,32 +1,60 @@
 import requests
 from django.shortcuts import render
-#Will import %20 after each word for the search adding to the url
+# Will import %20 after each word for the search adding to the url
 from requests.compat import quote_plus
 from bs4 import BeautifulSoup
 from . import models
 
 BASE_PROJECT_LIST_URL = 'https://losangeles.craigslist.org/search/bbb?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'base.html')
 
+
 def new_search(request):
     #Pull information from the search bar
-    search = request.POST.get('search')
-    #Gets the Search class model and retrieves the search form to the database
-    #models.Search.objects.create(search=search)
-    #will add the search within the url ''bbb?query={HERE}''
-    final_url = BASE_PROJECT_LIST_URL.format(quote_plus(search))
-    #soup = BeautifulSoup(data, features='html.parser')
+    search=request.POST.get('search')
+    # Gets the Search class model and retrieves the search form to the database
+    models.Search.objects.create(search=search)
+    # will add the search within the url ''bbb?query={HERE}''
+    final_url=BASE_PROJECT_LIST_URL.format(quote_plus(search))
+    response=requests.get(final_url)
+    data=response.text
+    soup=BeautifulSoup(data, features='html.parser')
+    post_titles=soup.find_all('a', {'class': 'result-title'})
+    print(post_titles)
+    response=requests.get(final_url)
+    data=response.text
 
-    print(final_url)
-    response = requests.get(final_url)
-    data = response.text
-    print(data)
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
 
 
-    frontend_stuff = {
+    frontend_stuff={
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', frontend_stuff)
